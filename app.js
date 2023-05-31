0cce7a261d334e76150d0ef16651370aa09af31f
krasi <krasimir.xristov@gmail.com>
2023-05-31 13:55:15 +0200

    create 404 route and message

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require('body-parser');
 
 const placesRoutes = require('./routes/places-routes');
+const HttpError = require("./models/http-error");
 
 const app = express();
 
@@ -10,13 +11,20 @@ app.use(bodyParser.json());
 
 app.use('/api/places', placesRoutes);
 
+app.use((req, res, next) => {
+
+    const error = new HttpError('Could not find this route.', 404);
+
+    throw error;
+});
+
 app.use((error, req, res, next) => {
-    if(res.headerSent) {
+    if (res.headerSent) {
         return next(error);
     }
 
     res.status(error.code || 500);
-    res.json({message: error.message || 'An unknown error occurred!'});
+    res.json({ message: error.message || 'An unknown error occurred!' });
 });
 
 
