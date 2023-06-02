a3c4d87d053aae292a37c8fda9ce7d8a75bde0c2
krasi <krasimir.xristov@gmail.com>
2023-06-02 23:39:25 +0200

    Clean my code from dummy data

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -1,17 +1,9 @@
-// const { v4: uuidv4, v4 } = require('uuid');
 const { validationResult } = require('express-validator');
 
 const HttpError = require('../models/http-error');
 const User = require('../models/user');
 
-const DUMMY_USERS = [
-    {
-        id: 'u1',
-        name: 'Krasimir Hristov',
-        email: 'test@test.com',
-        password: 'testers'
-    }
-];
+
 
 const getUsers = async (req, res, next) => {
 
