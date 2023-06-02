6b9d9652c911f68dde7ede3c607511a86733acbb
krasi <krasimir.xristov@gmail.com>
2023-06-02 21:25:28 +0200

    update title and description from place

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -26,7 +26,6 @@ const getPlaceById = async (req, res, next) => {
     const placeId = req.params.placeId;
 
     let place = null;
-
     try {
 
         place = await Place.findById(placeId);
@@ -108,25 +107,40 @@ const createPlace = async (req, res, next) => {
     res.status(201).json({ place: createdPlace });
 };
 
-const updatePlace = (req, res, next) => {
+const updatePlace = async (req, res, next) => {
 
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
+
         throw new HttpError('Invalid inputs passed, please check your data.', 422);
     };
 
     const { title, description } = req.body;
     const placeId = req.params.placeId;
 
-    const updatedPlace = { ...DUMMY_PLACES.find(place => place.id === placeId) };
-    const placeIndex = DUMMY_PLACES.findIndex(place => place.id === placeId);
-    updatedPlace.title = title;
-    updatedPlace.description = description;
+    let place = null;
+    try {
 
+        place = await Place.findById(placeId);
+    } catch (err) {
 
-    DUMMY_PLACES[placeIndex] = updatedPlace;
+        const error = new HttpError('Something wnt wrong, could not update place.', 500);
+        return next(error);
+    }
 
-    res.status(200).json({ place: updatedPlace });
+    place.title = title;
+    place.description = description;
+
+    try {
+
+        await place.save();
+    } catch (err) {
+
+        const error = new HttpError('Something wnt wrong, could not update place.', 500);
+        return next(error);
+    }
+
+    res.status(200).json({ place: place.toObject({ getters: true }) });
 };
 
 const deletePlace = (req, res, next) => {
