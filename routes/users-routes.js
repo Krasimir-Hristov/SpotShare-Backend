11c4abc9fbcf9d8f08fe08d96b22e62ddcd94be0
krasi <krasimir.xristov@gmail.com>
2023-06-04 21:23:34 +0200

    install and config (multer)

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { check } = require('express-validator');
 
 const usersControllers = require('../controllers/users-controllers');
+const fileUpload = require('../middleware/file-upload');
 
 const router = express.Router();
 
@@ -9,15 +10,17 @@ const router = express.Router();
 router.get('/', usersControllers.getUsers);
 
 
-router.post('/signup', [
-    check('name')
-        .trim()
-        .notEmpty(),
-    check('email')
-        .normalizeEmail()
-        .isEmail(),
-    check('password').isLength({ min: 6 })
-], usersControllers.signup);
+router.post('/signup',
+    fileUpload.single('image'),
+    [
+        check('name')
+            .trim()
+            .notEmpty(),
+        check('email')
+            .normalizeEmail()
+            .isEmail(),
+        check('password').isLength({ min: 6 })
+    ], usersControllers.signup);
 
 router.post('/login', usersControllers.login);
 
