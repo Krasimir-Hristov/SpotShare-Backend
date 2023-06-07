85eb656d4524ae90475c4f051ad350e6b2fb3a66
krasi <krasimir.xristov@gmail.com>
2023-06-07 20:27:39 +0200

    Create the upload image and not upload if is error

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+
 const express = require("express");
 require('dotenv').config();
 const bodyParser = require('body-parser');
@@ -12,16 +15,19 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use('/uploads/images', express.static(path.join('uploads', 'images')));
+
 app.use((req, res, next) => {
+
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
     );
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
-  
+
     next();
-  });
+});
 
 app.use('/api/places', placesRoutes);
 app.use('/api/users', usersRoutes);
@@ -34,7 +40,16 @@ app.use((req, res, next) => {
 });
 
 app.use((error, req, res, next) => {
+
+    if (req.file) {
+
+        fs.unlink(req.file.path, (err) => {
+            console.log(err);
+        });
+    }
+
     if (res.headerSent) {
+
         return next(error);
     }
 
