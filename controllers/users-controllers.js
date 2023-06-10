ac75ed1c67a6ca8595cfe6ffa23a9f0911279f4e
krasi <krasimir.xristov@gmail.com>
2023-06-10 18:22:39 +0200

    create a token with jsonwebtoken and conigurate it

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -1,5 +1,6 @@
 const { validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const HttpError = require('../models/http-error');
 const User = require('../models/user');
@@ -57,6 +58,7 @@ const signup = async (req, res, next) => {
     }
 
     const createdUser = new User({
+
         name,
         email,
         image: req.file.path,
@@ -65,14 +67,32 @@ const signup = async (req, res, next) => {
     });
 
     try {
+
         await createdUser.save();
     } catch (err) {
-        const error = new HttpError('Signing up failed, please try again.', 500);
 
+        const error = new HttpError('Signing up failed, please try again.', 500);
         return next(error);
     };
 
-    res.status(201).json({ user: createdUser.toObject({ getters: true }) });
+    let token = undefined;
+    try {
+
+        token = jwt.sign(
+            {
+                userId: createdUser.id,
+                email: createdUser.email
+            },
+            'spotshare_password',
+            { expiresIn: '1h' }
+        );
+    } catch (err) {
+
+        const error = new HttpError('Signing up failed, please try again.', 500);
+        return next(error);
+    }
+
+    res.status(201).json({ userId: createdUser.id, email: createdUser.email, token: token });
 };
 
 const login = async (req, res, next) => {
@@ -107,15 +127,34 @@ const login = async (req, res, next) => {
 
     if (!isValidPassword) {
 
-        
+
         const error = new HttpError('Invalid credentials, could not log you in.', 401);
+        return next(error);
+    }
+
+    
+    let token = undefined;
+    try {
+
+        token = jwt.sign(
+            {
+                userId: existingUser.id,
+                email: existingUser.email
+            },
+            'spotshare_password',
+            { expiresIn: '1h' }
+        );
+    } catch (err) {
+
+        const error = new HttpError('Logging up failed, please try again.', 500);
         return next(error);
     }
 
     res.json({
 
-        message: 'Logged in!',
-        user: existingUser.toObject({ getters: true })
+        userId: existingUser.id,
+        email: existingUser.email,
+        token: token
     });
 };
 
