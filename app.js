db636bf29290cd1c5d2f0a4c60fd531979299395
krasi <krasimir.xristov@gmail.com>
2023-06-12 21:35:24 +0200

    Update app.js

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ mongoose
         console.log('Database connected');
     })
     .then(() => {
-        app.listen(process.env.PORT);
+        app.listen(process.env.PORT || 5000);
         console.log('Server listen on: http://localhost:5000');
     })
     .catch(error => {
