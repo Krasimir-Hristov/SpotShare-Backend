896afdb9e4a5c6e87fa3b742c4858d0b8a65c5c7
krasi <krasimir.xristov@gmail.com>
2023-06-13 21:23:35 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,28 +4,36 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const placesRoutes = require('./routes/places-routes');
 const usersRoutes = require('./routes/users-routes');
 const HttpError = require('./models/http-error');
+
 require('dotenv').config();
-const cors = require('cors');
 
 const app = express();
 
-app.use(cors());
+// Middleware
 app.use(bodyParser.json());
 
+// Enable CORS
+app.use(cors());
+
+// Serve static files
 app.use('/uploads/images', express.static(path.join('uploads', 'images')));
 
+// Routes
 app.use('/api/places', placesRoutes);
 app.use('/api/users', usersRoutes);
 
+// Handle unknown routes
 app.use((req, res, next) => {
   const error = new HttpError('Could not find this route.', 404);
   throw error;
 });
 
+// Error handling middleware
 app.use((error, req, res, next) => {
   if (req.file) {
     fs.unlink(req.file.path, err => {
@@ -39,18 +47,21 @@ app.use((error, req, res, next) => {
   res.json({ message: error.message || 'An unknown error occurred!' });
 });
 
+// Connect to MongoDB and start the server
 mongoose
   .connect(process.env.MONGODB_URL, {
-    useNewUrlParser: true, 
-    useUnifiedTopology: true, 
-    useCreateIndex: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
   })
   .then(() => {
-    app.listen(5000);
-    console.log('connect to database');
+    app.listen(5000, () => {
+      console.log('Server is running on port 5000');
+    });
   })
-  .catch(err => {
-    console.log(err);
+  .catch(error => {
+    console.log(error);
   });
+
 
 
