c6775ec4c98bf2e23d49ccb5af3b2e1212433a6f
krasi <krasimir.xristov@gmail.com>
2023-06-13 18:05:04 +0200

    Update app.js

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ mongoose
         console.log('Database connected');
     })
     .then(() => {
-        app.listen(process.env.CYCLIC_URL || process.env.PORT);
+        app.listen();
         console.log('Server listen on: http://localhost:5000');
     })
     .catch(error => {
